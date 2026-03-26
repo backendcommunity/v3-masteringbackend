c5c5ffac59de58b75aed46121bbc2770ceca26fd
Solomon Eseme <kaperskyguru@gmail.com>
2026-03-26 13:55:18 +0100

    fix

--- components/pages/bootcamp-detail.tsx
+++ components/pages/bootcamp-detail.tsx
@@ -880,7 +880,7 @@ export function BootcampDetailPage({
                 >
                   <BadgeIcon className="mr-2 h-4 w-4" />
                   Enroll to Earn Certificate{" "}
-                  {bootcamp?.cohort?.allowsSubscription}
+                  {bootcamp?.cohort?.allowsSubscription + ""}
                 </Button>
               )}
             </CardContent>
