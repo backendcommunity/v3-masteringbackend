dc302f61ca2748f4e0d6a6aa09581e4afd79ee81
Solomon Eseme <kaperskyguru@gmail.com>
2026-05-06 01:40:54 +0100

    fix: remove project task summary

--- components/pages/project-task-detail.tsx
+++ components/pages/project-task-detail.tsx
@@ -250,7 +250,7 @@ export function ProjectTaskDetail({ slug, id, onNavigate }: ProjectTaskDetail) {
           <div className="flex items-center md:flex-row flex-col gap-3 md:gap-1 justify-between">
             <div className="flex items-center gap-2">
               <Button
-                onClick={() => handleShare(task?.title!, path)}
+                onClick={() => handleShare(task?.title!, path!)}
                 variant="outline"
                 size="sm"
               >
@@ -418,16 +418,6 @@ export function ProjectTaskDetail({ slug, id, onNavigate }: ProjectTaskDetail) {
                   </div>
                   <div className="flex-1 ">
                     <p className="text-sm font-medium">{pTask.title}</p>
-                    {pTask?.summary && (
-                      <div className="flex items-center gap-2 text-xs text-muted-foreground">
-                        <article
-                          className="text-muted-foreground leading-relaxed [&>*>table]:p-3 [&>*>table]:border [&>*>code]:rounded-xl [&>*>code]:bg-zinc-800 [&>*>code]:p-1 [&>*>code]:text-sm [&>*>code]:font-medium [&>*>code]:text-zinc-100 [&>*>code]:overflow-x-auto w-full [&>*>li>pre]:mt-5 [&>*>li>pre]:rounded-xl [&>*>li>pre]:bg-zinc-800 [&>*>li>pre]:p-4 [&>*>li>pre]:text-sm [&>*>li>pre]:font-medium [&>*>li>pre]:text-zinc-100 [&>*>li>pre]:overflow-x-auto [&>*>li>a]:text-amber-300 [&>p>a]:text-amber-300 mx-auto w-full text-zinc-700 dark:text-zinc-300 [&>pre]:overflow-x-auto [&>h2]:text-2xl [&>h2]:font-bold [&>h3]:text-xl [&>h3]:font-bold [&>p]:mt-2 [&>p]:leading-relaxed [&>pre]:mt-5 [&>pre]:rounded-xl [&>pre]:bg-zinc-800 [&>pre]:p-4 [&>pre]:text-sm [&>pre]:font-medium [&>pre]:text-zinc-100 [&>ul]:mt-5 [&>ul]:flex [&>ul]:list-disc [&>ul]:flex-col [&>ul]:gap-2 [&>ul]:pl-6 [&>ol]:mt-5 [&>ol]:flex [&>ol]:list-decimal [&>ol]:flex-col [&>ol]:gap-2 [&>ol]:pl-6 [&>*>span]:!text-black [&>p]:text-black dark:[&>*>span]:!text-muted-foreground dark:[&>p]:text-muted-foreground"
-                          dangerouslySetInnerHTML={{
-                            __html: pTask?.summary,
-                          }}
-                        ></article>
-                      </div>
-                    )}
                   </div>
                 </div>
               ))}
